29972be9caf86adf04bb15dbf01a6edbf7e7f958
Vincent Vaxelaire <68623931+VincentVaxelaire@users.noreply.github.com>
2023-03-23 11:50:28 +0100

    add popstate + rework the router
    I added a little bit of magic <3

---
+++ index.js
@@ -0,0 +1,23 @@
+import {router, navigateTo} from './src/router.js'
+import {canvas} from './src/canvas.js'
+
+canvas(document.querySelector('#webgl'));
+
+window.addEventListener('popstate', ()=> {
+    router()
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link=>{
+        link.addEventListener('click',(e) =>{
+            e.preventDefault()
+
+
+            navigateTo(e.target.href)
+        })
+    })
+
+    router()
+})
--- src/canvas.js
+++ src/canvas.js
@@ -1,13 +1,13 @@
 export function canvas(canvas) {
-    const color = ["black", "blue", "red"]
+    const color = ["black", "rgb(23, 98, 128)", "#690c0c"]
 
     window.addEventListener("navigation", event => {
         const { page } = event.detail;
-        if (page === "/index.html") {
+        if (page === "/") {
             canvas.style.backgroundColor = color[0];
-        } else if ( page === "/about.html") {
+        } else if (page === "/about") {
             canvas.style.backgroundColor = color[1];
-        } else if ( page === "/contact.html") {
+        } else if (page === "/contact") {
             canvas.style.backgroundColor = color[2];
         }
     });
