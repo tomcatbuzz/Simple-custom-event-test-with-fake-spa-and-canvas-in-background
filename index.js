af90cd05e4569b5878d7b32b9f474a858dd42020
Tony <tomcatbuzz@yahoo.com>
2023-03-27 14:26:41 -0400

    'wip'

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { router, navigateTo } from './src/router.js'
 import { canvas } from './src/canvas.js'
+import Transition from './src/transition.js'
+
+
+
+
 
 canvas(document.querySelector('#webgl'));
 
@@ -8,15 +13,34 @@ window.addEventListener('popstate', () => {
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    const transition = new Transition();
     const navLinks = document.querySelectorAll('[data-navigation]');
     navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
+        link.addEventListener('click', async (e) => {
             e.preventDefault()
+            // new code
+            const url = e.target.href;
+            const currentPage = window.location.pathname;
+            try {
+                await transition.show(currentPage, url, onComplete, onProgress);
+                await navigateTo(url)
+                await transition.hide()
+            } catch (error) {
+                console.error(error)
+                navigateTo('/404')
+            }
 
-            navigateTo(e.target.href)
+            // original
+            // navigateTo(e.target.href)
         })
     })
 
     router()
 })
+
+function onComplete() {
+    console.log('Transition complete');
+}
+function onProgress() {
+    console.log(`Transition progress: ${progress}`);
+}
--- src/canvas.js
+++ src/canvas.js
@@ -1,14 +1,72 @@
+import gsap from 'gsap'
 export function canvas(canvas) {
     const color = ["black", "rgb(23, 98, 128)", "#690c0c"]
 
+    // tl = gsap.timeline()
+    // tl.to('transition', { 
+    // duration: 1.2,
+    // x: '-100%',
+    // });
+    // return tl;
+
     window.addEventListener("navigation", event => {
         const { page } = event.detail;
+        // const div = document.createElement('div')
+        // div.className = 'transition'
+        // div.style.position = 'fixed'
+        // div.style.top = 0
+        // div.style.width = '100%'
+        // div.style.height = '100%'
+        // div.style.backgroundColor = 'white'
+        // div.style.zIndex = 2
+        // document.body.appendChild(div)
+        // const tl = gsap.timeline({
+        //     onComplete: () => document.body.removeChild(div)
+        // })
+        // const transition = document.getElementsByClassName('transition')
+        // tl.fromTo(div, { 
+        // duration: 1.8,
+        // ease: 'power1.inOut',
+        // x: '-100%',
+        // },
+        // {
+        // x: 0
+        // });
+        // tl.fromTo(div, {
+        //     x: 0
+        // },
+        // {
+        //     x: '100%'
+        // });
+        
+        // console.log(div, 'div')
+    
+        // const transition = event.navigation
+        // console.log(transition, 'transition')
+        
+
+        // const change = event.target.onchange
+        // console.log(event, 'event')
+        // console.log(change, 'onchange')
+        // console.log(event.currentTarget.ontransitionstart, 'start')
+        // console.log(event.currentTarget.ontransitionend, 'end')
+        // console.log(event.target, 'target')
+        // console.log(event.target.navigation.transition, 'nav')
+        // if(page && event.currentTarget.onchange) {
+        //     canvas.style.backgroundColor = color[0]
+        // } else if (page && event.currentTarget.onchange) {
+        //     canvas.style.backgroundColor = color[1];
+        // }
         if (page === "/") {
             canvas.style.backgroundColor = color[0];
+            // tl.play()
+            // classList.add('.show')
         } else if (page === "/about") {
             canvas.style.backgroundColor = color[1];
+            // tl.play()
         } else if (page === "/contact") {
             canvas.style.backgroundColor = color[2];
+            // tl.play()
         }
     });
 }
